6f8b79d466dd3a220299ee457ae9f4644cae0d69
Toria <ninetailedtori@uwu.gal>
2026-03-19 20:10:22 +0000

    Push some changes to stuff too.
    Signed-off-by: Toria <ninetailedtori@uwu.gal>

--- wc.lua
+++ wc.lua
@@ -1,4 +1,4 @@
-VERSION = "1.2.1"
+VERSION = "1.2.2"
 
 local micro = import("micro")
 local config = import("micro/config")
@@ -6,22 +6,28 @@ local util = import("micro/util")
 local utf8 = import("unicode/utf8")
 
 function init()
-    config.MakeCommand("wc", wordCount, config.NoComplete)
+    micro.SetStatusInfoFn("wc.w")
+    config.MakeCommand("wc", formatCount, config.NoComplete)
     config.AddRuntimeFile("wc", config.RTHelp, "help/wc.md")
-    config.TryBindKey("F5", "lua:wc.wordCount", false)
+    config.TryBindKey("F5", "lua:wc.wc", false)
 end
 
-function wordCount(bp)
+function formatCount(b)
+    local charCount, wordCount, lineCount = calc(b)
+    micro.InfoBar():Message("Lines:" .. lineCount .. "  Words:"..wordCount.."  Characters:"..charCount)
+end
+
+function calc(b)
     -- Buffer of selection/whole document
     local buffer
     --Get active cursor (to get selection)
-    local cursor = bp.Buf:GetActiveCursor()
+    local cursor = b.Buf:GetActiveCursor()
     --If cursor exists and there is selection, convert selection byte[] to string
     if cursor and cursor:HasSelection() then
         buffer = util.String(cursor:GetSelection())
     else
     --no selection, convert whole buffer byte[] to string
-        buffer = util.String(bp.Buf:Bytes())
+        buffer = util.String(b.Buf:Bytes())
     end
     --length of the buffer/selection (string), utf8 friendly
     charCount = utf8.RuneCountInString(buffer)
@@ -32,8 +38,21 @@ function wordCount(bp)
     local _, lineCount = buffer:gsub("\n", "")
     --add one to line count (since we're counting separators not lines above)
     lineCount = lineCount + 1
-    --display the message
-    micro.InfoBar():Message("Lines:" .. lineCount .. "  Words:"..wordCount.."  Characters:"..charCount)
-    -- number of substitutions, pattern: %w+ (more than one non-whitespace characters)
-    local _ , wordCount = buffer:gsub("%w+","")
+
+    return charCount, wordCount, lineCount
+end
+
+function cc(b)
+    local charCount, _, _ = calc(b)
+    return charCount
+end
+
+function wc(b)
+    local _, wordCount, _ = calc(b)
+    return wordCount
+end
+
+function lc(b)
+    local _, _, lineCount = calc(b)
+    return lineCount
 end
